8cfadab83699527e7c7c87f07b1e9d740f22a27e
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2021-06-13 20:01:42 +0200

    // Release

---
+++ IDS.Paderborn.FreeCATMA5/Annotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace IDS.Paderborn.FreeCATMA5
+{
+  public class Annotation
+  {
+    public string Annotator { get; set; }
+    public List<string> ValueChain { get; set; }
+    public string Value { get; set; }
+    public int From { get; set; }
+    public int To { get; set; }
+  }
+}
