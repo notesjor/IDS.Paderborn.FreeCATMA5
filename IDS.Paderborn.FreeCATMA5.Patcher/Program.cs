391cced5d0effa8a7f97ce40e2e7319768515c54
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2021-09-07 18:00:36 +0200

    // commit

---
+++ IDS.Paderborn.FreeCATMA5.Patcher/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using IDS.Paderborn.FreeCATMA5.Model.v2;
+using Newtonsoft.Json;
+
+namespace IDS.Paderborn.FreeCATMA5.Patcher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var res = new HashSet<string>();
+
+      foreach (var arg in args)
+      {
+        var files = Directory.GetFiles(arg, "*_v2_valid.json");
+        foreach (var file in files)
+        {
+          var doc = JsonConvert.DeserializeObject<Document>(File.ReadAllText(file, Encoding.UTF8));
+          foreach (var anno in doc.Annotations)
+          {
+            res.Add(anno.LayerValue);
+          }
+        }
+      }
+
+      File.WriteAllLines("fix.txt", res, Encoding.UTF8);
+    }
+  }
+}
