075a34bf61f76cc39cd8c78d80236f265003952f
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2021-09-07 16:21:35 +0200

    // commit

---
+++ IDS.Paderborn.FreeCATMA5.CorpusExplorer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using CorpusExplorer.Sdk.Ecosystem;
+
+namespace IDS.Paderborn.FreeCATMA5
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      CorpusExplorerEcosystem.InitializeMinimal();
+
+      foreach (var arg in args)
+      {
+        var files = Directory.GetFiles(arg, "*_v2_valid.json", SearchOption.TopDirectoryOnly);
+
+        foreach (var file in files)
+        {
+          try
+          {
+            var output = Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file) + ".cec6");
+            if (File.Exists(output))
+              continue;
+
+            var import = new CorpusExplorer.Sdk.Extern.Json.SimpleStandoff.SimpleJsonStandoffImporter();
+            var corpus = import.Execute(new[] {file}).First();
+            corpus.Save(output, false);
+          }
+          catch (Exception ex)
+          {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("");
+            Console.WriteLine(ex.StackTrace);
+            Console.WriteLine("-----");
+          }
+        }
+      }
+    }
+  }
+}
--- IDS.Paderborn.FreeCATMA5/Program.cs
+++ IDS.Paderborn.FreeCATMA5/Program.cs
@@ -15,17 +15,48 @@ namespace IDS.Paderborn.FreeCATMA5
   {
     static void Main(string[] args)
     {
-      var dirs = Directory.GetDirectories(args[0]);
-      foreach (var dir in dirs)
+      foreach (var arg in args)
       {
-        V1.Process(dir);
-        V2.Process(dir);
+        // Falls keine Unterverzeichnisse vorhanden sind, erstelle diese
+        var dirs = Directory.GetDirectories(arg);
+        if (dirs.Length == 0)
+          dirs = PackFilesInDirectories(arg);
+
+        // Konvertierung von Unterverzeichnissen
+        foreach (var dir in dirs)
+        {
+          V1.Process(dir);
+          V2.Process(dir);
+        }
       }
 
       Console.WriteLine("FERTIG!");
       Console.ReadLine();
     }
 
-    
+    private static string[] PackFilesInDirectories(string path)
+    {
+      var files = Directory.GetFiles(path);
+      foreach (var file in files)
+      {
+        var name = Path.GetFileNameWithoutExtension(file);
+        var split = name.Split(new[] {"-"}, StringSplitOptions.RemoveEmptyEntries);
+        var dir = "";
+
+        foreach (var x in split)
+        {
+          if (!x.StartsWith("0"))
+            continue;
+          dir = Path.Combine(path, x);
+          if (!Directory.Exists(dir))
+            Directory.CreateDirectory(dir);
+          break;
+        }
+
+        File.Move(file, Path.Combine(dir, Path.GetFileName(file)));
+      }
+
+      return Directory.GetDirectories(path);
+    }
   }
 }
